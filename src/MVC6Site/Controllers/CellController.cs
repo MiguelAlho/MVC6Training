03e2e859a7b1093d76bd849ddc61088efbdcfa7e
Miguel Alho <alho@miguelalho.com>
2015-05-12 11:52:52 +0100

    Added the Cell Controller and index page.

---
+++ src/MVC6Site/Controllers/CellController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using MVC6Site.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC6Site.Controllers
+{
+    public class CellController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<CellViewModel> cells = new List<CellViewModel>()
+            {
+                new CellViewModel() { Name = "W12345", Mcc=234, Mnc=15, Lac = 123, CellId = 12345 }
+                ,new CellViewModel() { Name = "W12346", Mcc=234, Mnc=15, Lac = 123, CellId = 12346 }
+            };
+
+            return View(cells);
+        }
+    }
+}
---
+++ src/MVC6Site/Models/CellViewModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6Site.Models
+{
+    public class CellViewModel
+    {
+        public string Name { get; set; }
+        public int Mcc { get; set; }
+        public int Mnc { get; set; }
+        public int Lac { get; set; }
+        public int CellId { get; set; }
+
+        public string Cgi
+        {
+            get
+            {
+                //return String.Format("{0}-{1}-{2}-{3}", Mcc, Mnc, Lac, CellId);
+                return $"{Mcc}-{Mnc}-{Lac}-{CellId}";
+            }
+        }
+    }
+}
