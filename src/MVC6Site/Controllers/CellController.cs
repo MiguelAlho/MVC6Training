ba9d0ef901dab8176490b30cb5f22e14ced60688
Miguel Alho <alho@miguelalho.com>
2015-05-13 16:41:00 +0100

    Added insert action for Cells
    CellRepo is now passed to controller through DI; added insert form for
    Cells

--- src/MVC6Site/Controllers/CellController.cs
+++ src/MVC6Site/Controllers/CellController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using MVC6Site.Models;
+using MVC6Site.Repositories;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,16 +12,40 @@ namespace MVC6Site.Controllers
 {
     public class CellController : Controller
     {
+        IRepository<CellViewModel> cellRepo;
+
+        public CellController(IRepository<CellViewModel> cellRepo)
+        {
+            this.cellRepo = cellRepo;
+        }
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<CellViewModel> cells = new List<CellViewModel>()
-            {
-                new CellViewModel() { Name = "W12345", Mcc=234, Mnc=15, Lac = 123, CellId = 12345 }
-                ,new CellViewModel() { Name = "W12346", Mcc=234, Mnc=15, Lac = 123, CellId = 12346 }
-            };
+            //IRepository<CellViewModel> cellRepo = new CellRepository();
 
-            return View(cells);
+            List<CellViewModel> cells = cellRepo.Find();
+
+            return View("Index", cells);
         }
+
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(CellViewModel viewModel)
+        {
+            cellRepo.Insert(viewModel);
+            return RedirectToAction("Index");
+
+            //return Index();
+        }
+
+
     }
+
+
 }
---
+++ src/MVC6Site/Repositories/CellRepository.cs
@@ -0,0 +1,33 @@
+﻿using MVC6Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6Site.Repositories
+{
+    public class CellRepository : IRepository<CellViewModel>
+    {
+        List<CellViewModel> cells = new List<CellViewModel>()
+        {
+            new CellViewModel() { Name = "W12345", Mcc=234, Mnc=15, Lac = 123, CellId = 12345 }
+            ,new CellViewModel() { Name = "W12346", Mcc=234, Mnc=15, Lac = 123, CellId = 12346 }
+        };
+
+        public List<CellViewModel> Find()
+        {
+            return cells;
+        }
+
+        public void Insert(CellViewModel viewModel)
+        {
+            cells.Add(viewModel);
+        }
+    }
+    // lets template
+    public interface IRepository<T> 
+    {
+        List<T> Find();
+        void Insert(CellViewModel viewModel);
+    }
+}
